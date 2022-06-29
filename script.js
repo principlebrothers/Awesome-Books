1a63a30524c459a934183f3c760a75450a7a19f2
principlebrothers <principlesbrothers@yahoo.com>
2022-06-29 08:25:02 +0000

    Add styles to index.html using Bootstrap

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const displayBook = document.querySelector('.display-Books');
-const addButton = document.querySelector('#addButton');
+const form = document.querySelector('form');
 
 function storedData(book) {
   localStorage.setItem('form', JSON.stringify(book));
@@ -21,18 +21,18 @@ class Book {
     this.title = title;
     this.author = author;
   }
+
   addBook() {
-    const id = books.length +1;
+    const id = books.length + 1;
 
     books.push(new Book(this.title, this.author, id));
     return books;
   }
-  static removeBook(bookCollection,id) {
-    return bookCollection.filter((book) => book.id !== +id);  
-  }
-  
-}
 
+  static removeBook(bookCollection, id) {
+    return bookCollection.filter((book) => book.id !== +id);
+  }
+}
 
 function displayBooks() {
   const allBooks = getData();
@@ -40,29 +40,34 @@ function displayBooks() {
     books = allBooks;
   }
   displayBook.innerHTML = '';
+  let index = 1;
   books.forEach((book) => {
     displayBook.insertAdjacentHTML(
       'beforeend',
-      `<h3>${book.title}</h3> <h3>${book.author}</h3>
-    <button type='button' class='removeBtn' id=${book.id} title= '${book.title}'>Remove</button><hr/>`,
+      `<div class='d-flex justify-content-between p-1 ${index % 2 === 0 ? '' : 'bg-secondary text-white'}'><div class='d-flex'><h3>${book.title}
+      </h3> &nbsp; &nbsp; <h3>by</h3> &nbsp; &nbsp; <h3>${book.author}</h3></div>
+      <button type='button' class='removeBtn btn btn-danger' id=${book.id} title= '${book.title}'>Remove</button></div>`,
     );
+    index += 1;
   });
 }
-addButton.addEventListener('click', () => {
+
+form.addEventListener('submit', (e) => {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const newBook = new Book(title, author);
   storedData(newBook.addBook());
   displayBooks();
-  
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+  e.preventDefault();
 });
-
 
 displayBook.addEventListener('click', (e) => {
   if (e.target.classList.contains('removeBtn')) {
     const allBooks = getData();
-    const id = e.target.id;
-    const remainingBooks = Book.removeBook(allBooks,id);
+    const { id } = e.target;
+    const remainingBooks = Book.removeBook(allBooks, id);
     storedData(remainingBooks);
     displayBooks();
   }
