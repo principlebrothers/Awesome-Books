72aece53abb604040d68e9121bc3d659a3cdf347
principlebrothers <principlesbrothers@yahoo.com>
2022-06-29 14:37:47 +0000

    Add navigation color

--- script.js
+++ script.js
@@ -39,12 +39,12 @@ function displayBooks() {
   if (allBooks) {
     books = allBooks;
   }
-  displayBook.innerHTML = '';
+  displayBook.innerHTML = '<h1>All Awesome Books</h1>';
   let index = 1;
   books.forEach((book) => {
     displayBook.insertAdjacentHTML(
       'beforeend',
-      `<div class='d-flex justify-content-between p-1 ${index % 2 === 0 ? '' : 'bg-secondary text-white'}'><div class='d-flex'><h3>${book.title}
+      `<div class='d-flex justify-content-between p-1 ${index % 2 === 0 ? '' : 'bg-secondary text-white'} border border-dark border-2'><div class='d-flex'><h3>${book.title}
       </h3> &nbsp; &nbsp; <h3>by</h3> &nbsp; &nbsp; <h3>${book.author}</h3></div>
       <button type='button' class='removeBtn btn btn-danger' id=${book.id} title= '${book.title}'>Remove</button></div>`,
     );
@@ -82,6 +82,9 @@ const contactInfo = document.querySelector('#contactInfo');
 
 addNewNav.addEventListener('click', (e) => {
   e.preventDefault();
+  addNewNav.classList.add('active');
+  listNav.classList.remove('active');
+  contactNav.classList.remove('active');
   addNewBook.classList.remove('invisible');
   if (shelveBooks) shelveBooks.classList.add('invisible');
   if (contactInfo) contactInfo.classList.add('invisible');
@@ -89,6 +92,9 @@ addNewNav.addEventListener('click', (e) => {
 
 listNav.addEventListener('click', (e) => {
   e.preventDefault();
+  addNewNav.classList.remove('active');
+  listNav.classList.add('active');
+  contactNav.classList.remove('active');
   shelveBooks.classList.remove('invisible');
   if (addNewBook) addNewBook.classList.add('invisible');
   if (contactInfo) contactInfo.classList.add('invisible');
@@ -96,6 +102,9 @@ listNav.addEventListener('click', (e) => {
 
 contactNav.addEventListener('click', (e) => {
   e.preventDefault();
+  addNewNav.classList.remove('active');
+  listNav.classList.remove('active');
+  contactNav.classList.add('active');
   contactInfo.classList.remove('invisible');
   if (addNewBook) addNewBook.classList.add('invisible');
   if (shelveBooks) shelveBooks.classList.add('invisible');
